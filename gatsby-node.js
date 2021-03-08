31a1ca9e46d21bebf3ec02575eea067d979888c7
Andreas Loew <andreas@codeandweb.com>
2021-03-08 17:47:11 +0100

    Documented

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,6 +11,16 @@ exports.createPages = async ({graphql, actions}) => {
         },
     ]
 
+    // ------------------------------------------------------------------------
+    // with out the query below
+    // {
+    //   allSitePage(filter: {context: {dog: {}}}) exists
+    // }
+    //
+    // the filter disappears from the graphql
+    // as soon as you activate the query below:
+    //
+    /*
     const result = await graphql(`
     query {
           allMdx {
@@ -21,7 +31,11 @@ exports.createPages = async ({graphql, actions}) => {
     }
   `)
     console.log(JSON.stringify(result, null, 4))
-    
+
+     */
+    // ------------------------------------------------------------------------
+
+
     dogData.forEach(dog => {
         const data = {
             path: `/${dog.name}`,
